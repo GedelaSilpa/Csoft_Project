638f55c523ecb1f33592bc2a9021854d7c36f7d9
silpagedela <silpa.gedela@chandusoft.com>
2025-11-24 18:08:47 +0530

    Fix DB connection and update env + Dockerfile

--- project-root/app/db.php
+++ project-root/app/db.php
@@ -4,6 +4,7 @@ $host = getenv('DB_HOST') ?: '127.0.0.1';
 $db   = getenv('DB_DATABASE') ?: 'local_db';
 $user = getenv('DB_USERNAME') ?: 'local_user';
 $pass = getenv('DB_PASSWORD') ?: 'local_pass';
+$port = getenv('DB_PORT') ?: '3306';
 $charset = 'utf8mb4';
 
 // PayPal Sandbox Credentials
@@ -11,11 +12,11 @@ $config = [
     'paypal' => [
         'client_id' => "AeMhb8ACVoPBMrfxnmZhUap9eIOAppJe71IAqOvxDuiuXMQwuWk_FUHCQOlCGmDsH9yt0-372L0axs0r",
         'secret'    => "EL0f-NdTLIsXP8ZcVJIrPJhwew9ec9WrIA3eiTGHvOAieoeR7O-4SMlXjE8W2Y2CAjBEv5s0bbKW4GTA",
-        'base_url'  => 'https://api-m.sandbox.paypal.com'
+        'base_url'  => "https://api-m.sandbox.paypal.com"
     ]
 ];
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
